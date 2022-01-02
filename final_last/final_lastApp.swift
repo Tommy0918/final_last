f975d2649cb1d3a3b38c039e9ee690317d068f98
Tommy <tommy890918@gmail.com>
2022-01-02 16:59:43 +0800

    Initial Commit

---
+++ final_last/final_lastApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_lastApp.swift
+//  final_last
+//
+//  Created by 吳承軒 on 2022/1/2.
+//
+
+import SwiftUI
+
+@main
+struct final_lastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
